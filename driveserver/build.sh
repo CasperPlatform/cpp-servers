ab0cca414798360281eda1fffa10acf293b49091
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-02 10:19:09 +0200

    Add main

--- driveserver/build.sh
+++ driveserver/build.sh
@@ -8,22 +8,30 @@
 # $MQQTINCLUDE/MQTTUnsubscribeServer.c $MQQTINCLUDE/MQTTUnsubscribeClient.c \
 # $MQQTINCLUDE/transport.c"
 
-SRCDIR=src
+cd $(dirname $0)
+
+SRCDIR="${PWD}/src"
+
+echo $SRCDIR
+
+
 
 if [ ! -d "$SRCDIR" ]; then
     echo "SRC-DIR Missing"
     exit 1
 fi    
 
-SRCFILES="$SRCDIR/serialhandler.c $SRCDIR/serialhandler.h
+SRCFILES="$SRCDIR/serialhandler.cpp $SRCDIR/serialhandler.hpp"
 
-COMMAND=g++ -o serialtest main.cpp -I$SRCFILES -lboost_system -lboost_date_time -lboost_thread
+COMMAND="c++ main.cpp -o build/serialtest  \
+-lboost_system -lboost_date_time -lboost_thread"
 
-COMMAND="g++ -o build/sens src/mqttClient.c \
--I/usr/local/include  "-I$MQQTINCLUDE/" "$MQQTADDITIONAL" \
--L/usr/local/lib "-L$MQQTINCLUDE/"  -lwiringPi"
+#COMMAND="gcc -o build/sens src/mqttClient.c \
+#-I/usr/local/include  "-I$MQQTINCLUDE/" "$MQQTADDITIONAL" \
+#-L/usr/local/lib "-L$MQQTINCLUDE/"  -lwiringPi"
 
-echo "*****running*****"
+
+echo "*****building*****"
 echo ""
 echo "$COMMAND"
 echo ""
