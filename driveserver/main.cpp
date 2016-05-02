7d108adba41e25d7bd557108c9715aebfcde532d
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-02 10:50:43 +0200

    lol

--- driveserver/main.cpp
+++ driveserver/main.cpp
@@ -10,10 +10,10 @@ int main(){
     printf("test");
 
 
-    SocketTest* socket = new SocketTest();
+    /*SocketTest* socket = new SocketTest();
 
     socket->startServer("127.0.0.1", "10000");
-
+*/
     return 0;
     
 }
