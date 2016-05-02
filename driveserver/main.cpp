998ec435e341ac493f0e8b055c5f6d570d461cc3
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-02 11:25:39 +0200

    prototype main.

--- driveserver/main.cpp
+++ driveserver/main.cpp
@@ -10,6 +10,21 @@ int main(){
     printf("test\n");
 
 
+    std::string comPort = "dev/cu.wchusbserial1420";
+
+    serialhandler sh;
+
+
+    if(!sh.start(comPort.c_str(), 9600))
+    {
+        return -1;
+    }
+
+    sh.write_string("Bajs\n");
+
+    sleep(5);
+
+    sh.stop();
     /*SocketTest* socket = new SocketTest();
 
     socket->startServer("127.0.0.1", "10000");
