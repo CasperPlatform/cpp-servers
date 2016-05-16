684c58644831943cfa5abd62bc7499d819c0ca53
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-16 11:14:26 +0200

    include libjpeg

--- videoserver/build.sh
+++ videoserver/build.sh
@@ -12,7 +12,7 @@ fi
 SRCFILES="$SRCDIR/videoserver.cpp $SRCDIR/sockethandler.cpp $SRCDIR/camerahandler.cpp"
 
 COMMAND="g++ -std=c++11 main.cpp $SRCFILES -I/usr/local/include -I/usr/include -I/usr/local -I$SRCDIR \
- -lboost_system -lboost_thread-mt -lpthread -lraspicam -lmmal -lmmal_core -lmmal_util \
+ -ljpeg -lboost_system -lboost_thread-mt -lpthread -lraspicam -lmmal -lmmal_core -lmmal_util \
  -L/usr/local/lib -L/usr/lib -L/opt/vc/lib -L/usr/local/Cellar/boost/1.59.0/lib -o build/videoserver"
 
 echo "*****building*****"
