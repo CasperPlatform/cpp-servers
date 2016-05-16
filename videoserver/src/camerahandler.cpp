a1bca5a9ef4b20c5a8f2140ef7e050e5ce16043c
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-16 11:27:35 +0200

    updated sleep.

--- videoserver/src/camerahandler.cpp
+++ videoserver/src/camerahandler.cpp
@@ -15,7 +15,7 @@ camerahandler::camerahandler()
     //wait a while until camera stabilizes
     std::cout<<"Sleeping for 3 secs"<<std::endl;
     
-    usleep(3000);
+    usleep(3000000);
     
     //capture
     Camera.grab();
