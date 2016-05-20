328d6e7a041f869d169d2ae992ede6ffd9b9e5d7
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-20 13:43:51 +0200

    removed mt

--- videoserver/build.sh
+++ videoserver/build.sh
@@ -12,8 +12,8 @@ fi
 SRCFILES="$SRCDIR/videoserver.cpp $SRCDIR/sockethandler.cpp $SRCDIR/camerahandler.cpp"
 
 COMMAND="g++ -std=c++11 main.cpp $SRCFILES -I/usr/local/include -I/usr/include -I/usr/local -I$SRCDIR \
- -lsqlite3 -ljpeg -lboost_system -lboost_thread-mt -lpthread -lraspicam -lmmal -lmmal_core -lmmal_util \
- -L/usr/local/lib -L/usr/lib -L/opt/vc/lib -L/usr/local/Cellar/boost/1.59.0/lib -o build/videoserver"
+ -lsqlite3 -ljpeg -lboost_system -lboost_thread -lpthread -lraspicam -lmmal -lmmal_core -lmmal_util \
+ -L/usr/local/lib -L/usr/lib -L/opt/vc/lib -o build/videoserver"
 
 echo "*****building*****"
 echo ""
