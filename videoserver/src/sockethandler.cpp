630b859fbd56dde10a2b5001170af8819660f6f6
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-23 13:28:17 +0200

    git blame corroleaus.

--- videoserver/src/sockethandler.cpp
+++ videoserver/src/sockethandler.cpp
@@ -68,7 +68,7 @@ void sockethandler::sendFrame(camerahandler::frame imageFrame)
 
     boost::shared_ptr<std::string> message(new std::string(header, header + 11));
 
-    std::cout << remote_endpoint.addres().to_string() << std::endl;
+    std::cout << remote_endpoint.address().to_string() << std::endl;
     
     socket.async_send_to(boost::asio::buffer(*message), remote_endpoint,
         boost::bind(&sockethandler::handle_send, this, message,
